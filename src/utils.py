2a076eb41385d9940b2e9afef505649fba5024b2
Victor Dutto <victor.dutto@epita.fr>
2021-11-22 19:46:27 +0100

    extracting functions into modules + version on dependencies

--- src/utils.py
+++ src/utils.py
@@ -1,30 +1,116 @@
-from datasets import ClassLabel, Sequence
-import random
-from IPython.display import display, HTML
+MAX_LENGTH = 384 # The maximum length of a feature
+DOC_STRIDE = 128
 
-def show_random_elements(dataset, num_examples=10):
+def prepare_train_features(examples, tokenizer):
     '''
-    Shows num_examples random elements from given datasets.
+    Prepare train features from given examples
 
             Parameters:
-                    dataset: a data of type datasets.arrow_dataset.Dataset 
-                    num_examples: An integer
+                    examples: a dict, from an datasets.arrow_dataset.Dataset
+                    tokenizer: A model tokenizer from the transformer library
 
             Returns:
-                    None
+                    transformers.tokenization_utils_base.BatchEncoding
+                    
     '''
-    assert num_examples <= len(dataset), "Can't pick more elements than there are in the dataset."
-    picks = []
-    for _ in range(num_examples):
-        pick = random.randint(0, len(dataset)-1)
-        while pick in picks:
-            pick = random.randint(0, len(dataset)-1)
-        picks.append(pick)
-    
-    df = pd.DataFrame(dataset[picks])
-    for column, typ in dataset.features.items():
-        if isinstance(typ, ClassLabel):
-            df[column] = df[column].transform(lambda i: typ.names[i])
-        elif isinstance(typ, Sequence) and isinstance(typ.feature, ClassLabel):
-            df[column] = df[column].transform(lambda x: [typ.feature.names[i] for i in x])
-    display(HTML(df.to_html()))
+    global MAX_LENGTH
+    global DOC_STRIDE
+    pad_on_right = tokenizer.padding_side == "right"
+    examples["question"] = [q.lstrip() for q in examples["question"]]
+
+    tokenized_examples = tokenizer(
+        examples["question" if pad_on_right else "context"],
+        examples["context" if pad_on_right else "question"],
+        truncation="only_second" if pad_on_right else "only_first",
+        max_length=MAX_LENGTH,
+        stride=DOC_STRIDE,
+        return_overflowing_tokens=True,
+        return_offsets_mapping=True,
+        padding="max_length",
+    )
+
+    sample_mapping = tokenized_examples.pop("overflow_to_sample_mapping")
+    offset_mapping = tokenized_examples.pop("offset_mapping")
+    tokenized_examples["start_positions"] = []
+    tokenized_examples["end_positions"] = []
+
+    for i, offsets in enumerate(offset_mapping):
+        input_ids = tokenized_examples["input_ids"][i]
+        cls_index = input_ids.index(tokenizer.cls_token_id)
+        sequence_ids = tokenized_examples.sequence_ids(i)
+        sample_index = sample_mapping[i]
+        answers = examples["answers"][sample_index]
+
+        if len(answers["answer_start"]) == 0:
+            tokenized_examples["start_positions"].append(cls_index)
+            tokenized_examples["end_positions"].append(cls_index)
+        else:
+            start_char = answers["answer_start"][0]
+            end_char = start_char + len(answers["text"][0])
+
+            token_start_index = 0
+            while sequence_ids[token_start_index] != (1 if pad_on_right else 0):
+                token_start_index += 1
+
+            token_end_index = len(input_ids) - 1
+            while sequence_ids[token_end_index] != (1 if pad_on_right else 0):
+                token_end_index -= 1
+
+            if not (offsets[token_start_index][0] <= start_char and offsets[token_end_index][1] >= end_char):
+                tokenized_examples["start_positions"].append(cls_index)
+                tokenized_examples["end_positions"].append(cls_index)
+            else:
+                while token_start_index < len(offsets) and offsets[token_start_index][0] <= start_char:
+                    token_start_index += 1
+                tokenized_examples["start_positions"].append(token_start_index - 1)
+                while offsets[token_end_index][1] >= end_char:
+                    token_end_index -= 1
+                tokenized_examples["end_positions"].append(token_end_index + 1)
+
+    return tokenized_examples
+
+def prepare_validation_features(examples, tokenizer):
+    '''
+    Prepare validation features from given examples
+
+            Parameters:
+                    examples: a dict, from an datasets.arrow_dataset.Dataset
+                    tokenizer: A model tokenizer from the transformer library
+
+            Returns:
+                    transformers.tokenization_utils_base.BatchEncoding
+                    
+    '''
+
+    pad_on_right = tokenizer.padding_side == "right" 
+    examples["question"] = [q.lstrip() for q in examples["question"]]
+
+    global MAX_LENGTH
+    global DOC_STRIDE
+
+    tokenized_examples = tokenizer(
+        examples["question" if pad_on_right else "context"],
+        examples["context" if pad_on_right else "question"],
+        truncation="only_second" if pad_on_right else "only_first",
+        max_length=MAX_LENGTH,
+        stride=DOC_STRIDE,
+        return_overflowing_tokens=True,
+        return_offsets_mapping=True,
+        padding="max_length",
+    )
+
+    sample_mapping = tokenized_examples.pop("overflow_to_sample_mapping")
+    tokenized_examples["example_id"] = []
+
+    for i in range(len(tokenized_examples["input_ids"])):
+        sequence_ids = tokenized_examples.sequence_ids(i)
+        context_index = 1 if pad_on_right else 0
+        sample_index = sample_mapping[i]
+        tokenized_examples["example_id"].append(examples["id"][sample_index])
+
+        tokenized_examples["offset_mapping"][i] = [
+            (o if sequence_ids[k] == context_index else None)
+            for k, o in enumerate(tokenized_examples["offset_mapping"][i])
+        ]
+
+    return tokenized_examples
