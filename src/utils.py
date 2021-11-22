d98eebe170825a4291b63ca9337ffdcdd1081cf0
Victor Dutto <victor.dutto@epita.fr>
2021-11-22 18:38:11 +0100

    [INIT]: Runned question_answering notebooks from huggingface

---
+++ src/utils.py
@@ -0,0 +1,30 @@
+from datasets import ClassLabel, Sequence
+import random
+from IPython.display import display, HTML
+
+def show_random_elements(dataset, num_examples=10):
+    '''
+    Shows num_examples random elements from given datasets.
+
+            Parameters:
+                    dataset: a data of type datasets.arrow_dataset.Dataset 
+                    num_examples: An integer
+
+            Returns:
+                    None
+    '''
+    assert num_examples <= len(dataset), "Can't pick more elements than there are in the dataset."
+    picks = []
+    for _ in range(num_examples):
+        pick = random.randint(0, len(dataset)-1)
+        while pick in picks:
+            pick = random.randint(0, len(dataset)-1)
+        picks.append(pick)
+    
+    df = pd.DataFrame(dataset[picks])
+    for column, typ in dataset.features.items():
+        if isinstance(typ, ClassLabel):
+            df[column] = df[column].transform(lambda i: typ.names[i])
+        elif isinstance(typ, Sequence) and isinstance(typ.feature, ClassLabel):
+            df[column] = df[column].transform(lambda x: [typ.feature.names[i] for i in x])
+    display(HTML(df.to_html()))
